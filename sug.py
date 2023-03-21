1657fae98ba17933e5569172c03855962f5e543f
Gaurav Prajapat <117170702+gauravprajapat29@users.noreply.github.com>
2023-03-21 10:57:18 +0530

    Add files via upload

--- sug.py
+++ sug.py
@@ -2,48 +2,47 @@ from nltk.collocations import TrigramCollocationFinder # Word Prediction Module
 from nltk.tokenize import word_tokenize  # Word Tokenize Module
 import pandas as pd 
 
-def file_write(a): # File to Store Data
-    p = open("all_data.txt","a")
-    p.write(text)
-    p.close()
+def prd(text):
+        new_text = text
 
-def file_read():  # File Read and Store in Variable
-    q = open("all_data.txt","r")
-    data = q.read()
-    q.close()
-    return data
-    
-    
-text = ""
-for i in range(1,5):  # Enter Text 
-    var = input("Enter your Text : ")
-    text = text +" "+var
+        def file_read():  # File Read and Store in Variable
+            q = open("all_data.txt","r")
+            data = q.read()
+            q.close()
+            return data
 
-file_write(text)
-data_txt = file_read()
 
-# Word Prediction
-first,second,third,count = [],[],[],[]
-f = TrigramCollocationFinder.from_words(word_tokenize(data_txt))
-for i in f.ngram_fd.items():
-    try :
-        first.append(i[0][0])
-        second.append(i[0][1])
-        third.append(i[0][2])
-        count.append(i[1])
-    except :
-        pass
+        data_txt = file_read()
 
-# Make dataset 
-dataset = pd.DataFrame({"First":first,"Second":second,"Third":third,"Count":count})
-dataset = dataset.sort_values(by="Count",ascending=False)
+        # Word Prediction
+        first,second,third,count = [],[],[],[]
+        f = TrigramCollocationFinder.from_words(word_tokenize(data_txt))
+        for i in f.ngram_fd.items():
+            try :
+                first.append(i[0][0])
+                second.append(i[0][1])
+                third.append(i[0][2])
+                count.append(i[1])
+            except :
+                pass
 
-print()
-print()
+        # Make dataset
+        dataset = pd.DataFrame({"First":first,"Second":second,"Third":third,"Count":count})
+        dataset = dataset.sort_values(by="Count",ascending=False)
 
-# New text Insert
-new_text = input("enter new text : ")
-index_no = dataset[dataset['First'] == new_text].index[0]
+        index_no = dataset[dataset['First'] == new_text]
 
-# Word Prediction Output
-print(dataset["Second"][index_no],dataset["Third"][index_no])
+        if index_no.shape[0] > 1:
+            first_word = index_no["Second"][index_no["Second"].index[0]]
+            second_word = index_no["Second"][index_no["Second"].index[1]]
+            third_word =  index_no["Third"][index_no["Third"].index[0]]
+            return (first_word, second_word, third_word)
+        elif index_no.shape[0] == 1:
+            first_word = index_no["Second"][index_no["Second"].index[0]]
+            second_word = new_text
+            third_word =  index_no["Third"][index_no["Third"].index[0]]
+            return (first_word, second_word, third_word)
+        elif index_no.shape[0] == 0:
+            first_word , second_word, third_word = new_text,new_text,new_text
+            return (first_word, second_word, third_word)
+
